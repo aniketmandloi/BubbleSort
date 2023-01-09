edee6e61fc7947c6953fdce43653a9d6cf486646
Aniket Mandloi <91334272+aniketmandloi@users.noreply.github.com>
2023-01-09 16:29:13 +0530

    Create BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,25 @@
+def swap(arr, index_1, index_2):
+  temp = arr[index_1]
+  arr[index_1] = arr[index_2]
+  arr[index_2] = temp
+
+def bubble_sort_unoptimized(arr):
+  iteration_count = 0
+  for el in arr:
+    for index in range(len(arr) - 1):
+      iteration_count += 1
+      if arr[index] > arr[index + 1]:
+        swap(arr, index, index + 1)
+
+  
+
+def bubble_sort(arr):
+  iteration_count = 0
+  for i in range(len(arr)):
+    # iterate through unplaced elements
+    for idx in range(len(arr) - i - 1):
+      iteration_count += 1
+      if arr[idx] > arr[idx + 1]:
+        # replacement for swap function
+        arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+        
